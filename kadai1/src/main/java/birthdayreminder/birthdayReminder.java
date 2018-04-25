fbcc7ce6f74c34aaeef66f856a3fbf46eed43b4e
konojunya <junya03dance@gmail.com>
2018-04-25 14:58:10 +0900

    できた

---
+++ kadai1/src/main/java/birthdayreminder/birthdayReminder.java
@@ -0,0 +1,30 @@
+package birthdayreminder;
+
+import birthdayreminder.GirlFriend;
+
+public class birthdayReminder {
+  public static void main(String[] args) {
+    GirlFriend[] girls = { new GirlFriend("まゆみ", 1988, 1, 5), new GirlFriend("さちこ", 1993, 5, 15),
+        new GirlFriend("まい", 1986, 5, 25), new GirlFriend("なみ", 1989, 8, 3), new GirlFriend("えり", 1991, 11, 19),
+        new GirlFriend("りえ", 1987, 12, 23) };
+
+    System.out.println("クラスの確認2:誕生日リマインダ");
+    for (int Month = 1; Month < 12; Month++) {
+      System.out.println(Month + "月");
+      for (GirlFriend girl : girls) {
+        if (Month == girl.getBirthMonth()) {
+          StringBuffer names = new StringBuffer();
+          names.append(girl.getName());
+          names.append("ちゃんが");
+          names.append(girl.getBirthDay());
+          names.append("日に誕生日です。");
+          names.append(girl.getAge());
+          names.append("歳になります。メッセージを送りましょう。");
+
+          System.out.println(names.toString());
+        }
+      }
+    }
+
+  }
+}
